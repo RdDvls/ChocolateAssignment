3a7c61b1667c8e0189cc7a2d89b1fb79d545e551
Clayton James Strickland <RdDvls@Clay.local>
2016-09-07 10:54:52 -0400

    Cleaned it up a bit

--- src/ChocolateMaker.java
+++ src/ChocolateMaker.java
@@ -3,25 +3,25 @@
  */
 public class ChocolateMaker {
     public int makeChocolate(int small, int big, int goal) {
-        {
-            int x = big * 5;
-            if (x == goal)
-                return 0;
-            else if (x < goal) {
-                if ((x + small) < goal)
-                    return -1;
-                else if ((x + small) == goal)
-                    return small;
-                else
-                    return goal - x;
-            } else {
-                while (x > goal) {
-                    x -= 5;
+        int x = big * 5;
+
+        if (x == goal)
+            return 0;
+        else if (x < goal) {
+            if ((x + small) < goal)
+                return -1;
+            else if ((x + small) == goal)
+                return small;
+            else
+                return goal - x;
+        } else {
+            while (x > goal) {
+                x -= 5;
                 }
-                if (x + small < goal)
-                    return -1;
-                else
-                    return goal - x;
+            if (x + small < goal)
+                return -1;
+            else
+                return goal - x;
             }
         }
     }
